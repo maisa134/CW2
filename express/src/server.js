6174b20315dc57730c358fc337027f005cc19828
maisa134 <73913853+maisa134@users.noreply.github.com>
2021-05-20 20:59:55 +0100

    added more middlware

--- express/src/server.js
+++ express/src/server.js
@@ -16,6 +16,29 @@ app.post('/hello', (req, res) => {
 app.use('/images', express.static(path.join(__dirname, '../assets')));
 
 
+let demoLogger = (req, res, next) => {
+    let current_datetime = new Date();
+    let formatted_date =
+      current_datetime.getFullYear() +
+      "-" +
+      (current_datetime.getMonth() + 1) +
+      "-" +
+      current_datetime.getDate() +
+      " " +
+      current_datetime.getHours() +
+      ":" +
+      current_datetime.getMinutes() +
+      ":" +
+      current_datetime.getSeconds();
+    let method = req.method;
+    let url = req.url;
+    let status = res.statusCode;
+    let log = `[${formatted_date}] ${method}:${url} ${status}`;
+    console.log(log);
+    next();
+};
+app.use(demoLogger);
+
 
 app.get('/lessons', async (req, res) => {
     const client = await MongoClient.connect(url,
